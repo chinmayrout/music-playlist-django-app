c41dd120c534f7de6cc9fd72dfa00022ddea04d5
Chinmay Rout <routchinmayy@gmail.com>
2024-02-28 15:00:39 +0530

    Added playlist view

--- music-playlist-app/django_project/django_project/settings.py
+++ music-playlist-app/django_project/django_project/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Add static file directories
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
